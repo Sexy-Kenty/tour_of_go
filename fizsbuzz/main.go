f76961e3b4f1d8c4d608d6484e3795190422aa94
hiramoto-kensuke <hiramoto.kensuke@moneyforward.co.jp>
2020-02-17 21:01:23 +0900

    defer statement

--- fizsbuzz/main.go
+++ fizsbuzz/main.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
+// defer へ渡した関数の実行を、呼び出し元の関数の終わり(returnする)まで遅延させる。
+// defer へ渡した関数の引数は、すぐに評価されるが、その関数自体は呼び出し元の関数がreturnするまで実行されない。
 func main() {
-	t := time.Now()
-	fmt.Println(t)
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good Morning")
-	case t.Hour() < 17:
-		fmt.Println("Good Afternoon")
-	default:
-		fmt.Println("Good Evening")
-	}
+	defer fmt.Println("World!")
+
+	fmt.Println("Hello")
 }
