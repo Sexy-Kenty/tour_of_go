d7da28032d13a991ba3f0a66c6f80556f23ef23f
hiramoto-kensuke <hiramoto.kensuke@moneyforward.co.jp>
2020-03-08 23:03:23 +0900

    rich html for twitter

---
+++ firebase_study/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+	e.GET("/tweets", tweets)
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func tweets(c echo.Context) error {
+	value := c.QueryParam("value")
+	api := connectTwitterApi()
+	//検索
+	searchResult, _ := api.GetSearch(`"`+value+`"`, nil)
+	tweets := make([]*TweetTemplate, 0)
+	for _, data := range searchResult.Statuses {
+		tweet := new(TweetTemplate)
+		tweet.Text = data.FullText
+		tweet.User = data.User.Name
+		tweet.Id = data.User.IdStr
+		tweet.ScreenName = data.User.ScreenName
+		tweet.Date = data.CreatedAt
+		tweet.TweetId = data.IdStr
+		tweets = append(tweets, tweet)
+	}
+
+	return c.Render(http.StatusOK, "tweets.html", tweets)
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+// TweetTempete はツイートの情報
+type TweetTemplate struct {
+	User       string `json:"user"`
+	Text       string `json:"text"`
+	ScreenName string `json:"screenName"`
+	Id         string `json:"id"`
+	Date       string `json:"date"`
+	TweetId    string `json:"tweetId"`
+}
+
+func connectTwitterApi() *anaconda.TwitterApi {
+	// Json読み込み
+	raw, error := ioutil.ReadFile("path/to/twitterAccount.json")
+	if error != nil {
+		fmt.Println(error.Error())
+		return nil
+	}
+
+	var twitterAccount TwitterAccount
+	// 構造体にセット
+	json.Unmarshal(raw, &twitterAccount)
+
+	// 認証
+	return anaconda.NewTwitterApiWithCredentials(twitterAccount.AccessToken, twitterAccount.AccessTokenSecret, twitterAccount.ConsumerKey, twitterAccount.ConsumerSecret)
+}
+
+// TwitterAccount はTwitterの認証用の情報
+type TwitterAccount struct {
+	AccessToken       string `json:"accessToken"`
+	AccessTokenSecret string `json:"accessTokenSecret"`
+	ConsumerKey       string `json:"consumerKey"`
+	ConsumerSecret    string `json:"consumerSecret"`
+}
